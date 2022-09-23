91090d3b0bd4198ce9352afe659b0ff029d185fe
Nika <white4ik1995@gmail.com>
2022-09-23 18:01:24 +0400

    initial commit

---
+++ src/components/Buttons.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Buttons = () => {
+  return (
+    <>
+    <div>
+        <ul>
+            <li><buttons className="border border-white py-2 px-6 rounded-full">Login</buttons></li>
+            <li><buttons className="btn-light bg-white py-2 px-4 rounded-full">Sign Up</buttons></li>
+        </ul>
+    </div>
+    </>
+  )
+}
+
+export default Buttons
